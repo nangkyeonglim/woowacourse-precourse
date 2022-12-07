6cb026bf10bf417094cf29df8dfadbefcbcacbdc
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-07 17:18:46 +0900

    refactor(Validator): 상수화 적용

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -1,13 +1,15 @@
+const { NUMBER, ERROR, GAME_COMMAND } = require("../constants/constants");
+
 class Validator {
   static checkUserNumber(userNumber) {
-    if (!this.#isNumber(userNumber)) throw new Error('[ERROR] 숫자만 입력해주세요');
-    if (!this.#isRangeOfLength(userNumber)) throw new Error('[ERROR] 3자리 숫자만 입력가능합니다');
-    if (this.#hasZero(userNumber)) throw new Error('[ERROR] 1 ~ 9사이의 숫자만 입력 가능합니다');
-    if (this.#isDuplicated(userNumber)) throw new Error('[ERROR] 중복되지 않는 숫자를 입력해주세요');
+    if (!this.#isNumber(userNumber)) throw new Error(ERROR.isNotNumber);
+    if (!this.#isRangeOfLength(userNumber)) throw new Error(ERROR.isNotRangeOfLength);
+    if (this.#hasZero(userNumber)) throw new Error(ERROR.hasZero);
+    if (this.#isDuplicated(userNumber)) throw new Error(ERROR.isDuplicated);
   }
 
   static checkGameCommand(gameCommand) {
-    if (!this.#isValidCommand(gameCommand)) throw new Error('[ERROR] 재시작: 1, 종료: 2 만 입력가능합니다')
+    if (!this.#isValidCommand(gameCommand)) throw new Error(ERROR.invalidCommand)
   }
 
   static #isNumber(number) {
@@ -24,11 +26,11 @@ class Validator {
   }
 
   static #isRangeOfLength(number) {
-    return number.length === 3;
+    return number.length === NUMBER.length;
   }
 
   static #isValidCommand(gameCommand) {
-    return gameCommand === '1' || gameCommand === '2';
+    return gameCommand === GAME_COMMAND.retry || gameCommand === GAME_COMMAND.quit;
   }
 }
 
