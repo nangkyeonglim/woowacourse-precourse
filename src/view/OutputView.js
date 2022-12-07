402ae748ddf63b774e53bec7ffef225b7dc210c0
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-07 15:09:35 +0900

    feat(OutputView): 숫자 비교 결과를 출력하는 기능 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -3,6 +3,19 @@ const { Console } = require("@woowacourse/mission-utils");
 const OutputView = {
   printIntialMessage() {
     Console.print('숫자 야구 게임을 시작합니다.');
+  },
+
+  printCompareResult(result) {
+    const { ball, strike } = result;
+    if (ball === 0 && strike === 0) {
+      Console.print(`낫싱`);
+      return;
+    }
+    let ballAndStirke = [];
+    if (ball > 0) ballAndStirke.push(`${ball}볼`);
+    if (strike > 0) ballAndStirke.push(`${strike}스트라이크`);
+
+    Console.print(ballAndStirke.join(' '))
   }
 };
 
