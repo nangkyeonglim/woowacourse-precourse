824c1071c6d514912b6b0a50db2f8f83451ebd25
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-07 16:49:31 +0900

    feat(Validator): 유저 넘버 유효성 검사 기능 구현

---
+++ src/utils/Validator.js
@@ -0,0 +1,34 @@
+class Validator {
+  static checkUserNumber(userNumber) {
+    if (!this.#isNumber()) throw new Error('[ERROR] 숫자만 입력해주세요');
+    if (!this.#isRangeOfLength()) throw new Error('[ERROR] 3자리 숫자만 입력가능합니다');
+    if (this.#hasZero()) throw new Error('[ERROR] 1 ~ 9사이의 숫자만 입력 가능합니다');
+    if (this.#isDuplicated()) throw new Error('[ERROR] 중복되지 않는 숫자를 입력해주세요');
+  }
+
+  static checkGameCommand(gameCommand) {
+    
+  }
+
+  static #isNumber(number) {
+
+  }
+
+  static #hasZero(number) {
+
+  }
+
+  static #isDuplicated(number) {
+
+  }
+
+  static #isRangeOfLength(number) {
+
+  }
+
+  static #isValidCommand(gameCommand) {
+
+  }
+}
+
+module.exports = Validator;
