3767ff46644c13fb971e471a889a1289d401243e
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-07 15:12:08 +0900

    feat(InputView): 재시작 여부를 입력받는 기능 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -3,6 +3,10 @@ const { Console } = require("@woowacourse/mission-utils");
 const InputView = {
   readUserNumber(callback) {
     Console.readLine('숫자를 입력해 주세요: ', callback);
+  },
+
+  readGameCommand(callback) {
+    Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요\n', callback);
   }
 };
 
