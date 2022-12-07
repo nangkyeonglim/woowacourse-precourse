99cca9443e2441a88dda76e7f34979a554537629
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-07 15:26:16 +0900

    refactor(baseBallGame): 컴퓨터 숫자를 필드로 가지도록 변경

--- src/models/BaseballGame.js
+++ src/models/BaseballGame.js
@@ -1,13 +1,19 @@
 class BaseballGame {
-  compareNumber(userNumber, computerNumber) {
+  #computerNumber;
+
+  constructor(computerNumber) {
+    this.#computerNumber = computerNumber;
+  }
+  
+  compareNumber(userNumber) {
     const result = {
       strike: 0,
       ball: 0,
     };
-    
+
     userNumber.forEach((num, index) => {
-      if (num === computerNumber[index]) result.strike += 1;
-      else if (computerNumber.includes(num)) result.ball += 1;
+      if (num === this.#computerNumber[index]) result.strike += 1;
+      else if (this.#computerNumber.includes(num)) result.ball += 1;
     });
     return result;
   }
