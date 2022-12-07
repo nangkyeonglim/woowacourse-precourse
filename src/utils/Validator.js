5da3e62a0997a77f124b3084aee7e2b9f7541d05
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-07 16:59:10 +0900

    fix(Validator): 파라미터 생략 오류 수정

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -1,13 +1,13 @@
 class Validator {
   static checkUserNumber(userNumber) {
-    if (!this.#isNumber()) throw new Error('[ERROR] 숫자만 입력해주세요');
-    if (!this.#isRangeOfLength()) throw new Error('[ERROR] 3자리 숫자만 입력가능합니다');
-    if (this.#hasZero()) throw new Error('[ERROR] 1 ~ 9사이의 숫자만 입력 가능합니다');
-    if (this.#isDuplicated()) throw new Error('[ERROR] 중복되지 않는 숫자를 입력해주세요');
+    if (!this.#isNumber(userNumber)) throw new Error('[ERROR] 숫자만 입력해주세요');
+    if (!this.#isRangeOfLength(userNumber)) throw new Error('[ERROR] 3자리 숫자만 입력가능합니다');
+    if (this.#hasZero(userNumber)) throw new Error('[ERROR] 1 ~ 9사이의 숫자만 입력 가능합니다');
+    if (this.#isDuplicated(userNumber)) throw new Error('[ERROR] 중복되지 않는 숫자를 입력해주세요');
   }
 
   static checkGameCommand(gameCommand) {
-    if (!this.#isValidCommand) throw new Error('[ERROR] 재시작: 1, 종료: 2 만 입력가능합니다')
+    if (!this.#isValidCommand(gameCommand)) throw new Error('[ERROR] 재시작: 1, 종료: 2 만 입력가능합니다')
   }
 
   static #isNumber(number) {
