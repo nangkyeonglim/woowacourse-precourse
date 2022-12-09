a77697699d33c64446a74d5c1d4a0988cf3e3832
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-10 00:28:47 +0900

    feat(App): 애플리케이션 시작부 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const BridgeGameController = require('./controller/BridgeGameController');
+
 class App {
-  play() {}
+  play() {
+    new BridgeGameController().start();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
