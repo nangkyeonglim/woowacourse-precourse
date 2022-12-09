69e390f64a94ab673b19c6be89a3b4ee1e30a807
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-09 17:29:29 +0900

    feat(LottoGame): 로또 게임 생성자 구현

--- src/models/LottoGame.js
+++ src/models/LottoGame.js
@@ -5,6 +5,10 @@ class LottoGame {
   #purchaseMoney;
   #lottos = [];
 
+  constructor(purchaseMoney) {
+    this.#purchaseMoney = purchaseMoney;
+  }
+
   issueLottos() {
     while (this.#lottos.length !== this.#purchaseMoney / 1000)
       this.#lottos.push(new Lotto(LottoNumberGeneartor.generate()));
