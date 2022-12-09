d888e1947db9618d19ec5fad20fea37d0545205d
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-09 22:58:36 +0900

    feat(InputView): 이동할 칸 입력받는 기능 구현

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -5,7 +5,9 @@ const InputView = {
     Console.readLine('다리의 길이를 입력해주세요.\n', callback);
   },
 
-  readMoving() {},
+  readMoving(callback) {
+    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', callback);
+  },
 
   readGameCommand() {},
 };
