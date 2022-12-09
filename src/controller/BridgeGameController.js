62af8746cf6119758e55feb5ecbca86e9b1ee306
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-09 23:12:18 +0900

    feat(BridgeGameController): 다리 길이 입력부 로직 구현

--- src/controller/BridgeGameController.js
+++ src/controller/BridgeGameController.js
@@ -1,8 +1,14 @@
+const InputView = require('../views/InputView');
 const OutputView = require('../views/OutputView');
 
 class BridgeGameController {
   start() {
     OutputView.printInitialMessage();
+    this.#readBridgeSizePhase();
+  }
+
+  #readBridgeSizePhase() {
+    InputView.readBridgeSize();
   }
 }
 
