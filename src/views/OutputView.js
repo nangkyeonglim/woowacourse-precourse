c365a75998785bd058b9f52bdc54da719240c168
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-10 00:57:14 +0900

    refactor(OutputView): 상수화 적용

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,21 +1,24 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { OUTPUT_MESSAGE, RESULT_MAP } = require('../constants/constants');
 
 const OutputView = {
   printInitialMessage() {
-    Console.print('다리 건너기 게임을 시작합니다.\n');
+    Console.print(OUTPUT_MESSAGE.initial);
   },
 
   printMap(resultMap) {
     resultMap.forEach((directionMap) => {
-      Console.print(`[ ${directionMap.join(' | ')} ]`);
+      Console.print(
+        RESULT_MAP.printOneLine(directionMap.join(RESULT_MAP.seperator))
+      );
     });
   },
 
   printResult(resultMap, success, attempts) {
-    Console.print('최종 게임 결과');
+    Console.print(OUTPUT_MESSAGE.resultTitle);
     this.printMap(resultMap);
-    Console.print(`게임 성공 여부: ${success ? '성공' : '실패'}`);
-    Console.print(`총 시도한 횟수: ${attempts}`);
+    Console.print(OUTPUT_MESSAGE.success(success));
+    Console.print(OUTPUT_MESSAGE.attempts(attempts));
   },
 
   close() {
