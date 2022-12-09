25e5dafa36ae357246f17e2dcceb85546db85b86
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-09 15:31:06 +0900

    feat(InputView): 당첨 번호 입력 기능 구현

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -4,6 +4,10 @@ const InputView = {
   readPurchaseMoney(callback) {
     Console.readLine('구입금액을 입력해 주세요.\n', callback);
   },
+
+  readWinningNumber(callback) {
+    Console.readLine('당첨 번호를 입력해 주세요.\n', callback);
+  },
 };
 
 module.exports = InputView;
