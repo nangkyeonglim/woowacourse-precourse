fc915403c8cc17be997b6c5c9e8cf43c8a3d1b3d
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-09 15:31:30 +0900

    feat(InputView): 보너스 번호 입력 기능 구현

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -8,6 +8,10 @@ const InputView = {
   readWinningNumber(callback) {
     Console.readLine('당첨 번호를 입력해 주세요.\n', callback);
   },
+
+  readBonusNumber(callback) {
+    Console.readLine('보너스 번호를 입력해 주세요.\n', callback);
+  },
 };
 
 module.exports = InputView;
