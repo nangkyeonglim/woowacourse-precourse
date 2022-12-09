56b8fdc2b8da445e5bb71be12fc71c83e18b7a62
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-10 01:12:24 +0900

    feat(Validator): 유효성 검증기 뼈대 코드 작성

---
+++ src/utils/Validator.js
@@ -0,0 +1,12 @@
+class Validator {
+  static checkBridgeSize(size) {}
+  static checkMoving(moving) {}
+  static checkGameCommand(gameCommand) {}
+
+  static #isNumber(number) {}
+  static #isRangeOfBridgeSize(size) {}
+  static #isValidMoving(moving) {}
+  static #isValidGameCommand(gameCommand) {}
+}
+
+module.exports = Validator;
