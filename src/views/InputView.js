6333290cbbebc161e278f1a3e92a7b6eb0c602c6
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-09 18:52:28 +0900

    refactor(InputView): 상수화 적용

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,16 +1,17 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { READ_MESSAGE } = require('../constants/constants');
 
 const InputView = {
   readPurchaseMoney(callback) {
-    Console.readLine('구입금액을 입력해 주세요.\n', callback);
+    Console.readLine(READ_MESSAGE.purchaseMoney, callback);
   },
 
   readWinningNumber(callback) {
-    Console.readLine('당첨 번호를 입력해 주세요.\n', callback);
+    Console.readLine(READ_MESSAGE.winningNumber, callback);
   },
 
   readBonusNumber(callback) {
-    Console.readLine('보너스 번호를 입력해 주세요.\n', callback);
+    Console.readLine(READ_MESSAGE.bonusNumber, callback);
   },
 };
 
