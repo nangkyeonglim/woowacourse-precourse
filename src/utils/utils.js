f758566168954ce025c520e3a2d5d672a7581c57
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-09 19:42:24 +0900

    feat(utils): 반올림 유틸 구현

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,4 +2,12 @@ const ascendingSort = (array) => {
   return array.sort((a, b) => a - b);
 };
 
-module.exports = { ascendingSort };
+const roundToTwo = (num) => {
+  return (+(Math.round(num + 'e+1') + 'e-1')).toFixed(1);
+};
+
+const localeString = (money) => {
+  return money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
+module.exports = { ascendingSort, roundToTwo, localeString };
