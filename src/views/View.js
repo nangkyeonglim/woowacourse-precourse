1e183a729409a4c96edc63fb68314b5a28b70e99
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-10 20:20:51 +0900

    feat(View): 뷰 인터페이스 작성

---
+++ src/views/View.js
@@ -0,0 +1,46 @@
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
+
+class View {
+  #input;
+  #output;
+
+  constructor() {
+    this.#input = InputView;
+    this.#output = OutputView;
+  }
+
+  readBridgeSize(callback) {
+    this.#input.readBridgeSize(callback);
+  }
+
+  readMoving(callback) {
+    this.#input.readMoving(callback);
+  }
+
+  readGameCommand(callback) {
+    this.#input.readGameCommand(callback);
+  }
+
+  printInitialMessage() {
+    this.#output.printInitialMessage();
+  }
+
+  printMap(resultMap) {
+    this.#output.printMap(resultMap);
+  }
+
+  printResult(object) {
+    this.#output.printResult(object);
+  }
+
+  printNewLine() {
+    this.#output.printNewLine();
+  }
+
+  close() {
+    this.#output.close();
+  }
+}
+
+module.exports = View;
